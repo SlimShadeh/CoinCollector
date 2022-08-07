f6ad8fac2d5d014c7742db49d3bda1e6d357a340
Tadija Ivkovic <96085801+SlimShadeh@users.noreply.github.com>
2022-08-07 17:22:11 +0200

    Add files via upload
    A game where you collect coins.

---
+++ Player.java
@@ -0,0 +1,24 @@
+package gui.coins;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Player extends Figure {
+
+	
+	
+	public Player(int x, int y, int width) {
+		super(x, y, width);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void paint(Graphics g) {
+		Color prevColor=g.getColor();
+		g.setColor(Color.RED);
+		g.drawLine(x, y-width/2, x, y+width/2-1);
+		g.drawLine(x-width/2, y, x+width/2-1, y);
+		g.setColor(prevColor);
+	}
+
+}
